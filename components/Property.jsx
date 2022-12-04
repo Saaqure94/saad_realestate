17ca14a0ac436d714aa3faf0351eba03d3682357
Saaqure94 <saaqure94@gmail.com>
2022-12-05 00:41:03 +1100

    fix: made change

--- components/Property.jsx
+++ components/Property.jsx
@@ -6,7 +6,7 @@ import { BsGridFill } from 'react-icons/bs';
 import { GoVerified } from 'react-icons/go';
 import millify from 'millify';
 
-import DefaultImage from '../assets/images/home.jpg';
+import DefaultImage from '../assets/images/home.JPG';
 
 const Property = ({ property: { coverPhoto, price, rentFrequency, rooms, title, baths, area, agency, isVerified, externalID } }) => (
     <Link href={`/property/${externalID}`} passHref>
